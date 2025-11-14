72e666dba62624019837b484b01e1a1ab1382547
Lyam Torres <lyamscott@hotmail.com>
2025-11-14 10:51:26 +0100

    Install TailwindCSS and Shadcn UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 
 function App() {
-  const [count, setCount] = useState(0)
   const [habits, setHabits] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
@@ -30,27 +28,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
       <div style={{ marginTop: 32 }}>
         <h2>Habits from API</h2>
         {loading && <p>Loading...</p>}
@@ -62,6 +39,9 @@ function App() {
             ))}
           </ul>
         )}
+      </div>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
       </div>
     </>
   )
